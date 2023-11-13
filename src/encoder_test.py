46c073fc74aa330637a26adec5419bddf713977d
wojt707 <dolibog.wojtek@gmail.com>
2023-11-13 14:49:11 +0100

    Reading data from encoder

---
+++ src/encoder_test.py
@@ -0,0 +1,21 @@
+# Sample code to demonstrate Encoder class.  Prints the value every 5 seconds, and also whenever it changes.
+
+import time
+import RPi.GPIO as GPIO
+from encoder import Encoder
+
+def valueChanged(value, direction):
+    print("* New value: {}, Direction: {}".format(value, direction))
+
+GPIO.setmode(GPIO.BCM)
+
+e1 = Encoder(27, 17, valueChanged)
+
+try:
+    while True:
+        time.sleep(5)
+        print("Value is {}".format(e1.getValue()))
+except Exception:
+    pass
+
+GPIO.cleanup()
--- src/motor_listener_node.py
+++ src/motor_listener_node.py
@@ -1,25 +1,69 @@
-import rospy
-from std_msgs.msg import Float32, String
-from motor_control import set_motor_speed, cleanup
-from speed_to_pwm import speed_to_pwm
+#!/usr/bin/env python
 
-pwm_range = 100 #wiem drugi raz to deklaruje
+## Simple talker demo that listens to std_msgs/Strings published 
+## to the 'chatter' topic
+
+import rospy
+from std_msgs.msg import String, Int32
+import RPi.GPIO as GPIO
+from time import sleep
+
+# dc motor pins 
+in1 = 24
+in2 = 23
+en = 25
+
+# dc motor setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(en, GPIO.OUT)
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.LOW)
+pwm = GPIO.PWM(en, 1000)
+pwm.start(0)
+
+def set_speed(speed):
+    if speed < 0:
+        speed = 0
+    elif speed > 100:
+        speed = 100
+    else:
+        pwm.ChangeDutyCycle(speed)
 
 def speed_direction_callback(data):
     print(data)
-    speed, reverse = data.data.split(',')
-    speed_kmph = float(speed)
+    speed_input, is_reverse_input = data.data.split(',')
+    speed = int(speed_input)
+    is_reverse = bool(int(is_reverse_input))
 
-    max_motor_speed_kmph = 10.0  # trza to dostosować
 
-    pwm_value = speed_to_pwm(speed_kmph, max_motor_speed_kmph, pwm_range)
 
-    set_motor_speed(pwm_value, reverse)
+    if reverse:
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.HIGH)
+        print("backward")
+    else:
+        GPIO.output(in1, GPIO.HIGH)
+        GPIO.output(in2, GPIO.LOW)
+        print("forward")
+    rospy.loginfo(f"Received speed: {speed}, is reverse: {is_reverse}")
+#     speed_kmph = float(speed)
+
+#     max_motor_speed_kmph = 10.0  # trza to dostosować
+
+#     pwm_value = speed_to_pwm(speed_kmph, max_motor_speed_kmph, pwm_range)
+
+#     set_motor_speed(pwm_value, reverse)
+
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber('chatter', String, speed_direction_callback)
+    rospy.spin()
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('motor_control', anonymous=True)
-        rospy.Subscriber('desired_speed_direction', String, speed_direction_callback)
-        rospy.spin()
+        listener()
     finally:
-        cleanup()
+        GPIO.cleanup()
