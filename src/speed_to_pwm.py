c1881f29e226e1d36c132a2f46c52e7c2dc67f93
wojt707 <dolibog.wojtek@gmail.com>
2023-11-16 23:56:26 +0100

    Folder restructuring

--- src/speed_to_pwm.py
+++
@@ -1,6 +0,0 @@
-def speed_to_pwm(speed_kmph, max_motor_speed_kmph, pwm_range):
-    if speed_kmph > max_motor_speed_kmph:
-        speed_kmph = actual_speed_kmph  #do dokonczenia max motor speed musi byc stale nie wiem moze ustawic na max rmp
-#ale max speed jest stałe tylko jest w motor_listener_node --wojt
-    pwm = (speed_kmph / max_motor_speed_kmph) * pwm_range
-    return int(pwm)
