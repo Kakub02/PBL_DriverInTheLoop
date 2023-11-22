57fc489e292e570be12c8b5dd856fa42322bec47
wojt707 <dolibog.wojtek@gmail.com>
2023-11-22 14:14:12 +0100

    pi controller

--- CodeKorea/pid_controller.py
+++ CodeKorea/pid_controller.py
@@ -1,15 +1,26 @@
 class PIDController:
-    def __init__(self, kp, ki, kd):
+    def __init__(self, kp, ki, kd, upper_limit, lower_limit):
         self.kp = kp
         self.ki = ki
         self.kd = kd
-        self.previousError = 0
-        self.integral = 0
+        self.upper_limit = upper_limit
+        self.lower_limit = lower_limit
+        #self.previousError = 0
+        self.integral_sum = 0
     
-    def calculate(self, setPointSpeed, realSpeed):
-        error = setPointSpeed - realSpeed
-        self.integral += error
-        derivative = error - self.previousError
-        output = self.kp * error + self.ki * self.integral + self.kd * derivative
+    def calculate(self, set_point_value, real_value):
+        error = set_point_value - real_value
+        self.integral_sum += error
+        #output = self.kp * error + self.ki * self.integral_sum
+
+        # calculating control value
+        cv = self.kp * (error + (self.kd/self.ki)*(self.integral_sum))
         self.previousError = error
-        return output
+        
+        if cv >= self.upper_limit:
+            cv = self.upper_limit
+            self.integral_sum = self.integral_sum - error
+        elif cv <= self.lower_limit:
+            cv = self.lower_limit
+            self.integral_sum = self.integral_sum - error
+        return cv
