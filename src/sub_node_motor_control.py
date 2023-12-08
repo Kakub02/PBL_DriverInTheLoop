f73914abc326f57f0b8c056e5121ba8b49a19301
Yulekk13 <102365739+Yulekk13@users.noreply.github.com>
2023-12-08 12:18:38 +0100

    trzeba robic

--- src/sub_node_motor_control.py
+++ src/sub_node_motor_control.py
@@ -13,21 +13,15 @@ motor_in1 = 23
 motor_in2 = 24
 motor_ena = 25
 #Motor 2
-motor_in3 = 23
-motor_in4 = 24
-motor_enb = 25
-
-#encoder
-encoder_pin = 1
+motor_in3 = 17
+motor_in4 = 27
+motor_enb = 22
 
 
 # Set GPIO pin numbers for encoder
-#Encoder 1
-left_encoder_pin = 17
-right_encoder_pin = 27
-#Encoder 2
-left_encoder_pin = 17
-right_encoder_pin = 27
+#Encoder
+left_encoder_pin = 8
+right_encoder_pin = 7
 
 # Set PID parameters
 kp=1.0
@@ -35,7 +29,7 @@ ki=0.1
 kd=0.01
 
 # Set maximum speed in kmph
-max_speed = 60
+max_speed = 100
 
 class Motor_Control(): # TODO main PID loop as in subscriber_node.py
 
