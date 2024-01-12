d1da86e98e53faa2fe77bb5d77f92d22f1cc01c4
wojt707 <dolibog.wojtek@gmail.com>
2024-01-12 23:33:03 +0100

    proper gitignore

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/jakub/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/jakub/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/jakub/catkin_ws/devel;/opt/ros/noetic' != '' else []
--- catkin_ws/build/dil_servo_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jakub/catkin_ws/src/dil_servo_controller
-# Build directory: /home/jakub/catkin_ws/build/dil_servo_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
