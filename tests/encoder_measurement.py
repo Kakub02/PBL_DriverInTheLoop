10aa4c38d63a320aefdbb99d74d2d09cf4544e4a
wojt707 <dolibog.wojtek@gmail.com>
2024-01-19 13:31:28 +0100

    measurements update

--- tests/encoder_measurement.py
+++ tests/encoder_measurement.py
@@ -7,42 +7,93 @@ left_pin = 69
 right_pin = 69  # dostosowac piny
 
 
+motor_in1 = 23
+motor_in2 = 24
+motor_ena = 25
+
+class DC_Motor:
+    # motor1 = Motor(23, 24, 25)
+    def __init__(self, in1, in2, en):
+        self.in1 = in1
+        self.in2 = in2
+        self.en = en
+
+        GPIO.setmode(GPIO.BCM)
+
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
+        GPIO.setup(self.en, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.en, 1000)
+        self.pwm.start(0)
+
+    def __del__(self):
+        self.pwm.stop()
+        GPIO.cleanup() # not sure if it should be here
+
+    def set_speed(self, setPointSpeed=0, reverse=0):
+        if reverse == 0:
+            GPIO.output(self.in1, GPIO.HIGH)
+            GPIO.output(self.in2, GPIO.LOW)
+        elif reverse == 1:
+            GPIO.output(self.in1, GPIO.LOW)
+            GPIO.output(self.in2, GPIO.HIGH)
+        self.pwm.ChangeDutyCycle(setPointSpeed)
+
+    def stop_motor(self):
+        GPIO.output(self.in1, GPIO.LOW)
+        GPIO.output(self.in2, GPIO.LOW)
+        self.pwm.ChangeDutyCycle(0)
+
+
 def measure_encoder():
+    motor1 = DC_Motor(motor_in1, motor_in2, motor_ena)
+    setpoint_pwm = 0
+
     frequency = 10  # in Hz
-    number_of_measurements = 500
+    number_of_measurements = 250
 
     encoder = Encoder_raw(left_pin, right_pin)
 
     start_time = time.time()
     elapsed_time = 0
     data_encoder = []
+    data_setpoint = []
+    data_both = []
 
     try:
-        for _ in range(number_of_measurements):
+        for loop_count in range(number_of_measurements):
+            print(loop_count)
             encoder_value = encoder.getValue()
 
             data_encoder.append({"time": elapsed_time, "value": encoder_value})
+            data_setpoint.append({"time": elapsed_time, "value": setpoint_pwm})
+            data_both.append({"time": elapsed_time, "encoder_value": encoder_value, "setpoint_pwm": setpoint_pwm})
 
             time.sleep(1 / frequency)
 
             elapsed_time = time.time() - start_time
+            if loop_count % 50 == 0:
+                setpoint_pwm += 10
+                motor1.set_speed(setpoint_pwm)
 
     except KeyboardInterrupt:
         pass  # Continue with plotting when the user interrupts the script
 
     draw_plot(data_encoder, "Time vs Encoder", "Time (s)", "Encoder value")
+    draw_plot(data_setpoint, "Time vs Setpoint", "Time (s)", "Setpoint")
+    draw_plot(data_both, "Time vs Encoder and setpoint", "Time (s)", "Values", ["encoder_value", "setpoint_speed"])
 
 
-def draw_plot(data, title, x_label, y_label):
+def draw_plot(data, title, x_label, y_label, legends):
     plot.figure()
-    plot.plot([entry["time"] for entry in data], [entry["value"] for entry in data], marker="o",linestyle="-", color="b")
+    for legend in legends:
+        plot.plot([entry["time"] for entry in data], [entry[legend] for entry in data], marker="o", linestyle="-", label=legend)
 
     plot.title(title)
     plot.xlabel(x_label)
     plot.ylabel(y_label)
-
+    plot.legend()
     plot.show()
-
 
 if __name__ == "__main__":
     GPIO.setmode(GPIO.BCM)
--- tests/imu_measurement.py
+++ tests/imu_measurement.py
@@ -1,11 +1,40 @@
 import time
 from mpu6050 import mpu6050
 import matplotlib.pyplot as plot
+import RPi.GPIO as GPIO
 
+# Set GPIO pin numbers for servos
+# Servo roll (boki)
+roll_servo_pin = 17
+# Servo pitch
+pitch_servo_pin = 27
+
+class Servo:
+    # servo = Servo(23)
+    def __init__(self, pwm_pin):
+        self.pwm_pin = pwm_pin
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pwm_pin, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.pwm_pin, 50)
+        self.pwm.start(0)
+
+    def __del__(self):
+        self.pwm.ChangeDutyCycle(7)
+        self.pwm.stop()
+        GPIO.cleanup() # not sure if it should be here
+
+    def set_duty_cycle(self, duty_cycle):
+        self.pwm.ChangeDutyCycle(duty_cycle)
 
 def measure_imu():
+    roll_servo = Servo(roll_servo_pin)
+    pitch_servo = Servo(pitch_servo_pin)
+    setpoint_roll_pwm = 5
+    setpoint_pitch_pwm = 5
+
     frequency = 10  # in Hz
-    number_of_measurements = 500
+    number_of_measurements = 250
 
     mpu = mpu6050(0x68)
 
@@ -13,34 +42,48 @@ def measure_imu():
     elapsed_time = 0
     data_x = []
     data_y = []
+    data_x_setpoint = []
+    data_y_setpoint = []
 
     try:
-        for _ in range(number_of_measurements):
+        for loop_count in range(number_of_measurements):
+            roll_servo.set_duty_cycle(setpoint_roll_pwm)
+            pitch_servo.set_duty_cycle(setpoint_pitch_pwm)
+            print(loop_count)
             accel_data = mpu.get_accel_data()
 
             data_x.append({"time": elapsed_time, "value": accel_data["x"]})
             data_y.append({"time": elapsed_time, "value": accel_data["y"]})
+            data_x_setpoint.append({"time": elapsed_time, "imu_x_value": accel_data["x"], "setpoint_roll_pwm": setpoint_roll_pwm})
+            data_y_setpoint.append({"time": elapsed_time, "imu_y_value": accel_data["y"], "setpoint_pitch_pwm": setpoint_pitch_pwm})
 
             time.sleep(1 / frequency)
 
             elapsed_time = time.time() - start_time
+            if loop_count % 100 == 0:
+                setpoint_roll_pwm += 1
+                setpoint_pitch_pwm += 1
+                roll_servo.set_duty_cycle(setpoint_roll_pwm)
+                pitch_servo.set_duty_cycle(setpoint_pitch_pwm)
     except KeyboardInterrupt:
         pass  # Continue with plotting when the user interrupts the script
 
     draw_plot(data_x, "Time vs Accelerometer X", "Time (s)", "Accel X (chuj wie jaka jednostka)")
     draw_plot(data_y, "Time vs Accelerometer Y", "Time (s)", "Accel Y (chuj wie jaka jednostka)")
+    draw_plot(data_x_setpoint, "Time vs Accelerometer X and setpoint_roll_pwm", "Time (s)", "Values", ["Accelerometer X", "setpoint_roll_pwm"])
+    draw_plot(data_y_setpoint, "Time vs Accelerometer Y and setpoint_roll_pwm", "Time (s)", "Values", ["Accelerometer Y", "setpoint_pitch_pwm"])
 
 
-def draw_plot(data, title, x_label, y_label):
+def draw_plot(data, title, x_label, y_label, legends):
     plot.figure()
-    plot.plot([entry["time"] for entry in data], [entry["value"] for entry in data], marker="o", linestyle="-", color="b")
+    for legend in legends:
+        plot.plot([entry["time"] for entry in data], [entry[legend] for entry in data], marker="o", linestyle="-", label=legend)
 
     plot.title(title)
     plot.xlabel(x_label)
     plot.ylabel(y_label)
-
+    plot.legend()
     plot.show()
-
 
 if __name__ == "__main__":
     measure_imu()
