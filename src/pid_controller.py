c0c789cd540ef3cf145df1b4bb4acfdcc862171d
wojt707 <dolibog.wojtek@gmail.com>
2024-02-14 22:09:18 +0100

    cleaning up trash files

--- src/pid_controller.py
+++ src/pid_controller.py
@@ -1,43 +1,29 @@
 class PID_controller:
-    # Controller gains 
-    Kp = 0.0
-    Ki = 0.0
-    Kd = 0.0
-
-   	# Derivative low-pass filter time constant
-    tau = 0.0
-
-    # Output limits
-    lower_limit = 0.0
-    upper_limit = 0.0
-
-    # Integrator limits
-    lower_int_limit = 0.0
-    upper_int_limit = 0.0
-
-	# Sample time (in seconds)
-    T = 0.0
-
-    # Controller memory
-    integrator = 0.0
-    previous_error = 0.0 # Required for integrator
-    differentiator = 0.0
-    previous_measurement = 0.0 # Required for differentiator
-
     def __init__(self, Kp, Ki, Kd, tau, lower_limit, upper_limit, lower_int_limit, upper_int_limit, T) -> None:
+        # Controller gains 
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
 
+   	    # Derivative low-pass filter time constant
         self.tau = tau # 1/2 sekundy
 
+        # Output limits
         self.lower_limit = lower_limit
         self.upper_limit = upper_limit
-
+        
+        # Integrator limits
         self.lower_int_limit = lower_int_limit
         self.upper_int_limit = upper_int_limit
         
+    	# Sample time (in seconds)
         self.T = T
+
+        # Controller memory
+        self.integrator = 0.0
+        self.previous_error = 0.0 # Required for integrator
+        self.differentiator = 0.0
+        self.previous_measurement = 0.0 # Required for differentiator
 
     # Function for calculating actual control value
     def update(self, setpoint, measurement):
